58671430c878609d348d2cf91cae79327d9f107c
8937 <yanjun@adpanshi.com>
2018-11-02 10:32:04 +0800

    master

--- git_sh.sh
+++ git_sh.sh
@@ -1,6 +1,15 @@
-echo "start"
 git add ./
+commit_content = "master"
+echo "5555"
+echo commit_content
+echo "$1"
+sleep 2
+if [ -z "$1" ]; then
+    echo "$1 is empty"
+fi
 git commit -m master
 git push
-
-echo "finish" 
+echo "333"
+sleep 2
+echo "555"
+sleep 2
