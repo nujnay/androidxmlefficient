d9bdac7db7490a171090a8d17631d942b95484b1
8937 <yanjun@adpanshi.com>
2018-11-02 16:02:10 +0800

    ggggggg

--- app/src/main/java/com/yanjun/androidxmlefficient/MainActivity.java
+++ app/src/main/java/com/yanjun/androidxmlefficient/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends Activity {
 
         setContentView(root);
 
-
 //        view = new View(this);
         view = LayoutInflater.from(this).inflate(R.layout.test, null);
         root.addView(view);
